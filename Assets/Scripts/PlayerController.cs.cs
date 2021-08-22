0cb4e1fee91d7cd5fa0c5bcd0f3224e339383b7b
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-22 15:25:25 -0500

    BustAMove

---
+++ Assets/Scripts/PlayerController.cs.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary> Starts up the Player </summary>
+public class PlayerController : MonoBehaviour
+{
+	public int score = 0;
+	public health = 5;
+	public float speed = 5.0F;
+
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	///<summary> WASD or Arrow Keys uesed </summary>
+	void FixedUpdate()
+	{
+		if ( Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if ( Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if ( Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if ( Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+	} 
+}
